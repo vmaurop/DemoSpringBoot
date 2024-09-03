662e0b92876400ef43fe7e6b35226b5b11f3325a
MAVROPOULOS Vaggelis <vmavropo@netcompany.com>
2024-09-03 15:46:51 +0300

    Adding actuator funcionality

--- src/main/java/com/luv2code/springboot/demo/mycollapp/rest/FunRestController.java
+++ src/main/java/com/luv2code/springboot/demo/mycollapp/rest/FunRestController.java
@@ -1,16 +1,32 @@
 package com.luv2code.springboot.demo.mycollapp.rest;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class FunRestController {
 
-    // expose "/" that retunr hello world
+    @Value("${coach.name}")
+    private String coachName;
+
+    @Value("${team.name}")
+    private String teamName;
 
     @GetMapping("/")
     public String sayHello() {
+        teamName = teamName + 1;
         return "Hello World:";
+    }
+
+    @GetMapping("/workout")
+    public String getDailyWorkout() {
+        return "Run a hard 5k";
+    }
+
+    @GetMapping("/fortune")
+    public String getDailyFotrune() {
+        return "Today is your lucky day.";
     }
 
 }
