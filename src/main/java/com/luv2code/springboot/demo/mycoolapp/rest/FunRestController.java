0cfafbb48c529cd722b8c679afaaa360c4870f92
MAVROPOULOS Vaggelis <vmavropo@netcompany.com>
2024-09-10 17:33:05 +0300

    Renaming packages

--- src/main/java/com/luv2code/springboot/demo/mycoolapp/rest/FunRestController.java
+++ src/main/java/com/luv2code/springboot/demo/mycoolapp/rest/FunRestController.java
@@ -1,4 +1,4 @@
-package com.luv2code.springboot.demo.mycollapp.rest;
+package com.luv2code.springboot.demo.mycoolapp.rest;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
--- src/test/java/com/luv2code/springboot/demo/mycoolapp/MycoolappApplicationTests.java
+++ src/test/java/com/luv2code/springboot/demo/mycoolapp/MycoolappApplicationTests.java
@@ -1,4 +1,4 @@
-package com.luv2code.springboot.demo.mycollapp;
+package com.luv2code.springboot.demo.mycoolapp;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
