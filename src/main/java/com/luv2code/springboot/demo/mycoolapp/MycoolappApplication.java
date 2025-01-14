3fa8a83ba9e72a9a8d4eb188b8e3a3810d596aaa
MAVROPOULOS Vaggelis <vmavropo@netcompany.com>
2025-01-14 17:14:02 +0200

    Adding dependencies for Hibernate,JPA

--- src/main/java/com/luv2code/springboot/demo/mycoolapp/MycoolappApplication.java
+++ src/main/java/com/luv2code/springboot/demo/mycoolapp/MycoolappApplication.java
@@ -1,7 +1,9 @@
 package com.luv2code.springboot.demo.mycoolapp;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 //@SpringBootApplication(
 //		scanBasePackages = {"com/luv2code/springboot/demo/mycoolapp"}
@@ -11,6 +13,13 @@ public class MycoolappApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MycoolappApplication.class, args);
+	}
+	
+	@Bean
+	public CommandLineRunner commandLineRunner(String[] args){
+		return runner -> {
+			System.out.println("Hello World");
+		};
 	}
 
 }
